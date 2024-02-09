08f72c8f21f6a9c6c1670093ac699a3e8d56ede5
AbdallahLearn <Abdallahjhn1@gmail.com>
2024-02-09 13:40:28 +0300

    display hello world on browser

---
+++ hello_world/src/main/java/com/hewaya/hello_world/model/Hobbyist.java
@@ -0,0 +1,26 @@
+package com.hewaya.hello_world.model;
+
+public class Hobbyist {
+    private String userName;
+    private String password;
+    public Hobbyist() {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
