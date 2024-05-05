4beb475a046b45219b6117580b3a30969c0965ab
Naif Ghannam <dhdfaui@gmail.com>
2024-05-05 16:52:30 +0300

    Update BillController.java

--- hello_world/src/main/java/com/hewayah/hello_world/controller/BillController.java
+++ hello_world/src/main/java/com/hewayah/hello_world/controller/BillController.java
@@ -1,4 +1,53 @@
 package com.hewayah.hello_world.controller;
 
+import com.hewayah.hello_world.entity.Bill;
+import com.hewayah.hello_world.service.BillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/bills")
 public class BillController {
+    private final BillService billService;
+
+    @Autowired
+    public BillController(BillService billService) {
+        this.billService = billService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Bill>> getAllBills() {
+        List<Bill> bills = billService.getAllBills();
+        return new ResponseEntity<>(bills, HttpStatus.OK);
+    }
+
+    @GetMapping("/{billId}")
+    public ResponseEntity<Bill> getBillById(@PathVariable Integer billId) {
+        Optional<Bill> bill = billService.getBillById(billId);
+        return bill.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Bill> createBill(@RequestBody Bill bill) {
+        Bill createdBill = billService.createBill(bill);
+        return new ResponseEntity<>(createdBill, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{billId}")
+    public ResponseEntity<Void> updateBill(@PathVariable Integer billId, @RequestBody Bill updatedBill) {
+        billService.updateBill(billId, updatedBill);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{billId}")
+    public ResponseEntity<Void> deleteBill(@PathVariable Integer billId) {
+        billService.deleteBill(billId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
